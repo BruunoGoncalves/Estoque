8ed1c377613d7462365928f52876a37d59508b97
Bruno Gonçalves <bruuno_goncalves@outlook.com.br>
2024-11-12 22:05:01 -0300

    att: Method Encapsulamento

--- course/src/entities/Product.java
+++ course/src/entities/Product.java
@@ -14,10 +14,31 @@ public class Product {
         this.quantity = quantity;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
     public Product(String name, double price) {
         this.name = name;
         this.price = price;
     }
+
 
     public double totalValueInStock() {
         return price * quantity;
